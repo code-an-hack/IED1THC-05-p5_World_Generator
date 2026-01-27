94467ab30a96cea3082c5077e5b385d4a45a188d
mahallavr <mahallavr2022@gmail.com>
2026-01-27 16:33:10 +0100

    order functions

--- sketch.js
+++ sketch.js
@@ -34,37 +34,6 @@ function setup() {
   drawIsland();
 }
 
-function getTextureForValue(normalizedValue) {
-  if (normalizedValue < thresholdWater) return textureWater;
-  if (normalizedValue < thresholdSand) return textureSand;
-  if (normalizedValue < thresholdGrass) return textureGrass;
-  return textureStone;
-}
-
-function getRadialFactor(columnIndex, rowIndex) {
-  const centerColumn = (gridColumns - 1) / 2;
-  const centerRow = (gridRows - 1) / 2;
-  const maxDistanceFromCenter = dist(0, 0, centerColumn, centerRow);
-  const distanceFromCenter = dist(columnIndex, rowIndex, centerColumn, centerRow);
-  const normalizedDistance = distanceFromCenter / maxDistanceFromCenter;
-
-  const start = radialGradientStart;
-  const end = radialGradientEnd;
-
-  if (normalizedDistance <= start) return 1;
-  if (normalizedDistance >= end) return 0;
-  return map(normalizedDistance, start, end, 1, 0);
-}
-
-function getCellValue(columnIndex, rowIndex) {
-  const noiseValue = noise(
-    columnIndex * perlinNoiseScale,
-    rowIndex * perlinNoiseScale
-  );
-  const radialFactor = getRadialFactor(columnIndex, rowIndex);
-  return constrain(noiseValue * radialFactor, 0, 1);
-}
-
 function generateGridValues() {
     for (let rowIndex = 0; rowIndex < gridRows; rowIndex++) {
     grid[rowIndex] = [];
@@ -89,4 +58,35 @@ function drawIsland() {
       );
     }
   }
+}
+
+function getCellValue(columnIndex, rowIndex) {
+    const noiseValue = noise(
+      columnIndex * perlinNoiseScale,
+      rowIndex * perlinNoiseScale
+    );
+    const radialFactor = getRadialFactor(columnIndex, rowIndex);
+    return constrain(noiseValue * radialFactor, 0, 1);
+  }
+  
+function getTextureForValue(normalizedValue) {
+  if (normalizedValue < thresholdWater) return textureWater;
+  if (normalizedValue < thresholdSand) return textureSand;
+  if (normalizedValue < thresholdGrass) return textureGrass;
+  return textureStone;
+}
+
+function getRadialFactor(columnIndex, rowIndex) {
+  const centerColumn = (gridColumns - 1) / 2;
+  const centerRow = (gridRows - 1) / 2;
+  const maxDistanceFromCenter = dist(0, 0, centerColumn, centerRow);
+  const distanceFromCenter = dist(columnIndex, rowIndex, centerColumn, centerRow);
+  const normalizedDistance = distanceFromCenter / maxDistanceFromCenter;
+
+  const start = radialGradientStart;
+  const end = radialGradientEnd;
+
+  if (normalizedDistance <= start) return 1;
+  if (normalizedDistance >= end) return 0;
+  return map(normalizedDistance, start, end, 1, 0);
 }
